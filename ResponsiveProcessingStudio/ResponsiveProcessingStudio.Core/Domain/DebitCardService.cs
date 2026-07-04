b4f2de2e8af8baa43d26d0c8b44e483810f5ca97
NAJE221 <artemigorevich113@gmail.com>
2026-07-04 19:41:10 +0300

    add bank services domain models and factory

---
+++ ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Domain/DebitCardService.cs
@@ -0,0 +1,8 @@
+using ResponsiveProcessingStudio.Core.Domain;
+
+public class DebitCardService : BankService
+{
+    public override ServiceType Type => ServiceType.DebitCard;
+    public override string DisplayName => "Банковская карта";
+    public override string RequiredDepartment => "Карточный отдел";
+}
---
+++ ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Domain/MoneyTransferService.cs
@@ -0,0 +1,8 @@
+using ResponsiveProcessingStudio.Core.Domain;
+
+public class MortgageService : BankService
+{
+    public override ServiceType Type => ServiceType.Mortgage;
+    public override string DisplayName => "Ипотека";
+    public override string RequiredDepartment => "Ипотечный отдел";
+}
