d7fb4b055814b5300a93f57438f660f249d40cfd
Alexsey Smolyaninov <aleksejsmolaninov40@gmail.com>
2026-07-06 12:40:37 +0300

    add RetryPolice

--- ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Domain/ServiceType.cs
+++ ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Domain/ServiceType.cs
@@ -3,9 +3,9 @@ namespace ResponsiveProcessingStudio.Core.Domain;
 public enum ServiceType
 {
     Unknown = 0,
-    Credit,
-    DebitCard,
-    Deposit,
-    Mortgage,
-    MoneyTransfer
+    Credit = 1,
+    DebitCard = 2,
+    Deposit = 3,
+    Mortgage = 4,
+    MoneyTransfer = 5
 }
