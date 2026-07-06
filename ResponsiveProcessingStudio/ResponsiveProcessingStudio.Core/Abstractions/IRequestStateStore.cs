db74c9ae7fd9b290c8ec68d5a276458955d69816
NULL-X9C <ThrxwnewExceptixn@proton.me>
2026-07-06 13:18:24 +0300

    fix(IRequestStateStore)

--- ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Abstractions/IRequestStateStore.cs
+++ ResponsiveProcessingStudio/ResponsiveProcessingStudio.Core/Abstractions/IRequestStateStore.cs
@@ -1,11 +1,12 @@
 using ResponsiveProcessingStudio.Core.Domain;
-using ResponsiveProcessingStudio.Core.Pipeline;
 
 namespace ResponsiveProcessingStudio.Core.Abstractions;
 
-public interface IPipelineMetrics
+public interface IRequestStateStore
 {
-    PipelineSnapshot GetSnapshot();
-    void OnStatusChanged(RequestStatus oldStatus, RequestStatus newStatus);
-    void IncrementRetry();
+    void Add(SupportRequest request);
+    void Update(SupportRequest request);
+    void UpdateStatus(Guid requestId, RequestStatus status);
+    IReadOnlyCollection<SupportRequest> GetAll();
+    IReadOnlyCollection<SupportRequest> GetRecent(int count);
 }
